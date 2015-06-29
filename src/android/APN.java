af6941ba8185bfb5ca3ab30d30150a02acfe84c6
sereneray <wangenglishkl@126.com>
2015-06-29 16:58:12 +0800

    first commit

---
+++ src/android/APN.java
@@ -0,0 +1,50 @@
+package com.eruipan.cordova.plugin.apn;
+
+public class APN {
+	 private String id;  
+	  
+     private String apn;  
+
+     private String type;  
+     
+
+    public String getId() {
+		return id;
+	}
+
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+
+	public String getApn() {
+		return apn;
+	}
+
+
+
+	public void setApn(String apn) {
+		this.apn = apn;
+	}
+
+
+
+	public String getType() {
+		return type;
+	}
+
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+
+	public String toString() {  
+         return "id=" + id + ",apn=" + apn + ";type=" + type;  
+     }  
+}
