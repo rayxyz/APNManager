e9f82895b6c036530c81151c08c70b3d5270edc1
sereneray <wangenglishkl@126.com>
2015-06-30 16:24:09 +0800

    fixed the bug of js file

--- www/APNManager.js
+++ www/APNManager.js
@@ -2,45 +2,39 @@
  * @author Wnag Rui
  * @email wangenglishkl@126.com
  */
-cordova.define("com.eruipan.cordova.plugin.apnmanager.APNManager", function(require, exports, module) { 
-var argscheck = require('cordova/argscheck'),
-    utils = require('cordova/utils'),
-    exec = require('cordova/exec');
 
 var APNManager = function() {};
 
-APNManager.echo = function(successCallback, erroCallback) {
-    exec(successCallback, erroCallback, "APNManager", "echo", ["Hello, navtive!"]);
+APNManager.prototype.echo = function(successCallback, erroCallback) {
+    cordova.exec(successCallback, erroCallback, "APNManager", "echo", ["Hello, navtive!"]);
 };
 
-APNManager.getCurrAPNId = function(successCallback, erroCallback) {
-    exec(successCallback, erroCallback, "APNManager", "getCurrAPNId", []);
+APNManager.prototype.getCurrAPNId = function(successCallback, erroCallback) {
+    cordova.exec(successCallback, erroCallback, "APNManager", "getCurrAPNId", []);
 };
 
-APNManager.getCurrAPNInfo = function(successCallback, erroCallback) {
-    exec(successCallback, erroCallback, "APNManager", "getCurrAPNInfo", []);
+APNManager.prototype.getCurrAPNInfo = function(successCallback, erroCallback) {
+    cordova.exec(successCallback, erroCallback, "APNManager", "getCurrAPNInfo", []);
 };
 
-APNManager.getAPNList = function(successCallback, erroCallback) {
-    exec(successCallback, erroCallback, "APNManager", "getAPNList", []);
+APNManager.prototype.getAPNList = function(successCallback, erroCallback) {
+    cordova.exec(successCallback, erroCallback, "APNManager", "getAPNList", []);
 };
 
-APNManager.getAvailableAPNList = function(successCallback, erroCallback) {
-    exec(successCallback, erroCallback, "APNManager", "getAvailableAPNList", []);
+APNManager.prototype.getAvailableAPNList = function(successCallback, erroCallback) {
+    cordova.exec(successCallback, erroCallback, "APNManager", "getAvailableAPNList", []);
 };
 
-APNManager.setPreferAPN = function(apnId, apnName, successCallback, erroCallback) {
-    exec(successCallback, erroCallback, "APNManager", "setPreferAPN", [apnId, apnName]);
+APNManager.prototype.setPreferAPN = function(apnId, apnName, successCallback, erroCallback) {
+    cordova.exec(successCallback, erroCallback, "APNManager", "setPreferAPN", [apnId, apnName]);
 };
 
-APNManager.addAPN = function(successCallback, erroCallback) {
-    exec(successCallback, erroCallback, "APNManager", "addAPN", []);
+APNManager.prototype.addAPN = function(successCallback, erroCallback) {
+    cordova.exec(successCallback, erroCallback, "APNManager", "addAPN", []);
 };
 
-APNManager.startAPNSettings = function(successCallback, errorCallback) {
-	exec(successCallback, errorCallback, "APNManager", "startAPNSettings", []);
+APNManager.prototype.startAPNSettings = function(successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "APNManager", "startAPNSettings", []);
 }
 
 module.exports = APNManager;
-
-});
